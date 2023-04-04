9f2a3f21c151d712a99112dae73495bd10fcf731
Victor Briones <victor.briones@praedicat.com>
2023-04-04 09:04:06 -0400

    init commit

--- src/test/java/Tests/demoGoogle/Test_WeatherChallenge.java
+++ src/test/java/Tests/demoGoogle/Test_WeatherChallenge.java
@@ -3,6 +3,7 @@ package Tests.demoGoogle;
 import Baseclass.BaseClass;
 import Helpers.Helpers;
 import Objects.Temperature;
+import Objects.Weather;
 import Pages.Page_GoogleSearch;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Parameters;
@@ -18,7 +19,7 @@ import java.net.URL;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Test_WeatherChallenge extends BaseClass {
-    private static final String API_URL = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s";
+
 
     @Test
     @Parameters({"weatherCity", "apiKey"})
@@ -40,21 +41,20 @@ public class Test_WeatherChallenge extends BaseClass {
 
         System.out.println(illapelTemperature.getTemperature());
 
-        String apiResponse = weatherAPICall(apiKey);
-        ObjectMapper objectMapper = new ObjectMapper();
-        Temperature temperature = objectMapper.readValue(apiResponse, Temperature.class);
+        Weather illapelWeather = new Weather(apiKey, weatherCity);
+
+// Print temperature information
+        System.out.println("Temperature: " + illapelWeather.getMain().getTemp());
+        System.out.println("Feels like: " + illapelWeather.getMain().getFeelsLike());
+        System.out.println("Min temperature: " + illapelWeather.getMain().getTempMin());
+        System.out.println("Max temperature: " + illapelWeather.getMain().getTempMax());
     }
 
-    public String weatherAPICall(String API_KEY) {
+    public String weatherAPICall(String API_KEY, String weatherCity) {
         try {
-            // Example location (New York City)
-            double lat = -31.63349;
-            double lon = -71.16967;
-
-            String apiUrl = String.format(API_URL, lat, lon, API_KEY);
+            String apiUrl = String.format(API_URL, weatherCity, API_KEY);
             HttpURLConnection connection = (HttpURLConnection) new URL(apiUrl).openConnection();
             connection.setRequestMethod("GET");
-
             BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String line;
             StringBuilder response = new StringBuilder();
@@ -62,9 +62,6 @@ public class Test_WeatherChallenge extends BaseClass {
                 response.append(line);
             }
             reader.close();
-
-            // Print the response from the API endpoint
-            System.out.println(response.toString());
             return response.toString();
         } catch (ProtocolException e) {
             throw new RuntimeException(e);
