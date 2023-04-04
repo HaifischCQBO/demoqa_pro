c5c845e1d6f0a20d15ebffa10a226d2a7665e8ec
Victor Briones <victor.briones@praedicat.com>
2023-04-04 01:15:51 -0400

    init commit

--- src/test/java/Tests/demoGoogle/Test_WeatherChallenge.java
+++ src/test/java/Tests/demoGoogle/Test_WeatherChallenge.java
@@ -8,14 +8,23 @@ import org.openqa.selenium.WebElement;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+
 public class Test_WeatherChallenge extends BaseClass {
+    private static final String API_URL = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s";
+
     @Test
-    @Parameters({"weatherCity"})
-    public void exampleTest(String weatherCity) {
+    @Parameters({"weatherCity", "apiKey"})
+    public void exampleTest(String weatherCity, String apiKey) {
 
         Helpers helpers = new Helpers(driver);
         Page_GoogleSearch page_googleSearch = new Page_GoogleSearch(driver);
-        WebElement searchInput =  page_googleSearch.getSearchInput();
+        WebElement searchInput = page_googleSearch.getSearchInput();
         helpers.sendText(searchInput, "Weather in " + weatherCity);
         helpers.Click(page_googleSearch.getSearchButton());
         Temperature illapelTemperature =
@@ -26,7 +35,37 @@ public class Test_WeatherChallenge extends BaseClass {
 
                         )
                 );
-        System.out.println(illapelTemperature.getTemperature());
 
+        System.out.println(illapelTemperature.getTemperature());
+        weatherAPICall(apiKey);
     }
+
+    public void weatherAPICall(String API_KEY) {
+        try {
+            // Example location (New York City)
+            double lat = -31.63349;
+            double lon = -71.16967;
+
+            String apiUrl = String.format(API_URL, lat, lon, API_KEY);
+            HttpURLConnection connection = (HttpURLConnection) new URL(apiUrl).openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            StringBuilder response = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+
+            // Print the response from the API endpoint
+            System.out.println(response.toString());
+
+        } catch (ProtocolException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
